c6956125347cfcb750609280e72c08a78626fc2b
NurbekAka <foto.nurbek@gmail.com>
2020-01-09 18:41:08 +0600

    Pull project

---
+++ blog/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Comment
+
+
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=255)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(required=False, widget=forms.Textarea)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('email', 'body')
